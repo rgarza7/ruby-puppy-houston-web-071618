dfe97c8223892ea007eb33840cd24222f16f0092
github username <github email address>
2018-07-28 19:02:23 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Dog
+
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+end
